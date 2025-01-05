11038f1a983729fc3dab3f1c79d6e657f2ccef27
NotYourTypicalDeveloper <carolineho1983@gmail.com>
2025-01-05 11:45:50 +0000

    Added some responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,7 @@ import "./App.scss";
 import { useState, useReducer } from "react";
 import { v4 as uuid } from "uuid";
 import Postit from "./components/Postit.jsx";
-
-const initialNotesState = [];
-const notesReducer = (prevState, action) => {
-  switch (action.type) {
-    // ADD NEW NOTE_____
-    case "ADD_NOTE": {
-      const newState = [...prevState, action.payload];
-      return newState;
-    }
-    // UPDATE EXISTING NOTE_____
-    case "UPDATE_NOTE": {
-      const indexToReplace = prevState.findIndex(
-        (elem) => elem.id === action.payload.id
-      );
-
-      const prevStateNotesSplice = [...prevState];
-      prevStateNotesSplice.splice(indexToReplace, 1, {
-        ...prevState[indexToReplace],
-        text: action.payload.text,
-      });
-      return prevStateNotesSplice;
-    }
-    // DELETE EXISTING NOTE_____
-    case "DELETE_NOTE": {
-      const newState = prevState.filter(
-        (note) => note.id !== action.payload.id
-      );
-      return newState;
-    }
-  }
-};
+import { notesReducer, initialNotesState } from "./utils/Reducer.js";
 
 function App() {
   const [noteInput, setNoteInput] = useState("");
---
+++ src/utils/Reducer.js
@@ -0,0 +1,30 @@
+export const initialNotesState = [];
+export const notesReducer = (prevState, action) => {
+  switch (action.type) {
+    // ADD NEW NOTE_____
+    case "ADD_NOTE": {
+      const newState = [...prevState, action.payload];
+      return newState;
+    }
+    // UPDATE EXISTING NOTE_____
+    case "UPDATE_NOTE": {
+      const indexToReplace = prevState.findIndex(
+        (elem) => elem.id === action.payload.id
+      );
+
+      const prevStateNotesSplice = [...prevState];
+      prevStateNotesSplice.splice(indexToReplace, 1, {
+        ...prevState[indexToReplace],
+        text: action.payload.text,
+      });
+      return prevStateNotesSplice;
+    }
+    // DELETE EXISTING NOTE_____
+    case "DELETE_NOTE": {
+      const newState = prevState.filter(
+        (note) => note.id !== action.payload.id
+      );
+      return newState;
+    }
+  }
+};
