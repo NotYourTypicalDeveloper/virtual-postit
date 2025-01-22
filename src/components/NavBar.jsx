771771415c7b77bd5e21d479f741b94d98f07393
NotYourTypicalDeveloper <carolineho1983@gmail.com>
2025-01-22 17:00:53 +0000

    updated guidelines

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,9 +36,19 @@ const NavBar = ({ toggleDrawer, dispatch }) => {
         </button>
         {isInfoOpen && (
           <div className="info-dropdown">
-            <p>Click "ADD" or press CMD + Enter keys to create a note.</p>
+            <p>
+              Click <b>ADD</b> or press <b>CMD + Enter</b> keys to create a
+              note.
+            </p>
             <p>
               Drag and drop post its to place them anywhere on the dashboard.
+            </p>
+            <p>
+              Delete all your current notes by clicking <b>reset dashboard</b>
+            </p>
+            <p>
+              Delete all your archived notes by clicking
+              <b>delete archived notes</b>
             </p>
             <p>Commands: </p>
 
