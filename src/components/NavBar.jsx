70aaf46c1c5017b1b209e07fb1a1ee02692534d2
Mugurel Mirica <mmugur81@gmail.com>
2025-02-02 17:25:33 +0000

    Added guidelines overlay to close it when clicking outside (#2)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,7 +16,7 @@ const NavBar = ({ toggleDrawer }) => {
   const numberOfArchivedNotes = notesState.filter(
     (note) => note.archived
   ).length;
-  const toggleInfo = () => {
+  const toggleGuidelines = () => {
     setIsInfoOpen((prevState) => !prevState);
   };
 
@@ -46,41 +46,43 @@ const NavBar = ({ toggleDrawer }) => {
         >
           reset dashboard
         </button>
-        <button className="navbar-btn" onClick={toggleInfo}>
+        <button className="navbar-btn" onClick={toggleGuidelines}>
           Guidelines <BadgeInfo />
         </button>
         {isInfoOpen && (
-          <div className="info-dropdown">
-            <p>
-              Click <b>ADD</b> or press <b>CMD + Enter</b> keys to create a
-              note.
-            </p>
-            <p>
-              Drag and drop post its to place them anywhere on the dashboard.
-            </p>
-            <p>
-              Delete all your current notes by clicking <b>reset dashboard</b>
-            </p>
-            <p>
-              Delete all your archived notes by clicking
-              <b>delete archived notes</b>
-            </p>
-            <p>Commands: </p>
+          <div className="info-overlay" onClick={toggleGuidelines}>
+            <div className="info-dropdown">
+              <p>
+                Click <b>ADD</b> or press <b>CMD + Enter</b> keys to create a
+                note.
+              </p>
+              <p>
+                Drag and drop post its to place them anywhere on the dashboard.
+              </p>
+              <p>
+                Delete all your current notes by clicking <b>reset dashboard</b>
+              </p>
+              <p>
+                Delete all your archived notes by clicking
+                <b>delete archived notes</b>
+              </p>
+              <p>Commands: </p>
 
-            <div className="info-item">
-              <CloseIcon />
-              <span>Delete note</span>
-            </div>
-            <div className="info-item">
-              <EditIcon />
-              <span>Edit Note</span>
-            </div>
-            <div className="info-item">
-              <ArchiveIcon />
-              <span>Archive Note</span>
-            </div>
-            <div className="info-item">
-              <Undo2 /> <span>Unarchive (restore) Note</span>
+              <div className="info-item">
+                <CloseIcon />
+                <span>Delete note</span>
+              </div>
+              <div className="info-item">
+                <EditIcon />
+                <span>Edit Note</span>
+              </div>
+              <div className="info-item">
+                <ArchiveIcon />
+                <span>Archive Note</span>
+              </div>
+              <div className="info-item">
+                <Undo2 /> <span>Unarchive (restore) Note</span>
+              </div>
             </div>
           </div>
         )}
